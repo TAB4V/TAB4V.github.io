6380e507a350c070acc70e71534ad267eaf581cc
Roman Kornilov <r.kornilov@lar.tech>
2020-05-27 19:17:36 +0300

    restore

--- main.js
+++ main.js
@@ -11,22 +11,12 @@ let clearButton = document.getElementById('clrBtn');
 
 // при нажатии на кнопку START
 startButton.addEventListener('click', function() {
-  var uuid = $('#startBtn').attr('data-uuid');
-  var value = $('#startBtn').attr('data-value');
-  var characteristic = charArray[uuid].characteristic;
-  var converted = new Uint8Array([value]);
-  characteristic.writeValue(converted);
-  log('Started');
+  log('start');
 });
 
 // при нажатии на кнопку STOP
 stopButton.addEventListener('click', function() {
-  var uuid = $('#stopBtn').attr('data-uuid');
-  var value = $('#stopBtn').attr('data-value');
-  var characteristic = charArray[uuid].characteristic;
-  var converted = new Uint8Array([value]);
-  characteristic.writeValue(converted);
-  log('Stoped');
+  log('stop');
 });
 
 // при нажатии на кнопку CLEAR
@@ -54,9 +44,8 @@ disconnectButton.addEventListener('click', function() {
 // Обработка события отправки формы
 sendForm.addEventListener('submit', function(event) {
   event.preventDefault(); // Предотвратить отправку формы
-  send();
-  // send(inputField.value); // Отправить содержимое текстового поля
-  // inputField.value = '';  // Обнулить текстовое поле
+  send(inputField.value); // Отправить содержимое текстового поля
+//  inputField.value = '';  // Обнулить текстовое поле
   inputField.focus();     // Вернуть фокус на текстовое поле
 });
 
@@ -65,12 +54,11 @@ let deviceCache = null;
 
 // Запустить выбор Bluetooth устройства и подключиться к выбранному
 function connect() {
-  var _dev = (deviceCache ? Promise.resolve(deviceCache) : requestBluetoothDevice());
-  _dev.then(device => showValues(device));
-  return _dev
-    .then(device => connectDeviceAndCacheCharacteristic(device))
-    .then(characteristic => startNotifications(characteristic))
-    .catch(error => log(error));
+  return (deviceCache ? Promise.resolve(deviceCache) :
+      requestBluetoothDevice()).
+      then(device => connectDeviceAndCacheCharacteristic(device)).
+      then(characteristic => startNotifications(characteristic)).
+      catch(error => log(error));
 }
 
 // Запрос выбора Bluetooth устройства
@@ -78,100 +66,39 @@ function requestBluetoothDevice() {
   log('Requesting bluetooth device...');
 
   return navigator.bluetooth.requestDevice({
-	  // acceptAllDevices: true,
-    filters: [
-	   // {services: [0xAA81]}
+//	  acceptAllDevices: true,
+      filters: [
+//	   {services: [0xAA81]}
 	   {namePrefix: 'TAB4V'}
 	  ],
-	  optionalServices: [0xAA80, 0xAA64]
-  }).then(device => {
-      log('"' + device.name + '" bluetooth device selected');
-      deviceCache = device;
+	  optionalServices: [0xAA80, 0xAA60]
+  }).
+      then(device => {
+        log('"' + device.name + '" bluetooth device selected');
+        deviceCache = device;
 
-      // Добавленная строка
-      deviceCache.addEventListener('gattserverdisconnected', handleDisconnection);
+        // Добавленная строка
+        deviceCache.addEventListener('gattserverdisconnected',
+            handleDisconnection);
 
-      return deviceCache;
-    });
+        return deviceCache;
+      });
 }
 
 // Обработчик разъединения
 function handleDisconnection(event) {
   let device = event.target;
 
-  log('"' + device.name + '" bluetooth device disconnected, trying to reconnect...');
+  log('"' + device.name +
+      '" bluetooth device disconnected, trying to reconnect...');
 
-  connectDeviceAndCacheCharacteristic(device)
-    .then(characteristic => startNotifications(characteristic))
-    .catch(error => log(error));
+  connectDeviceAndCacheCharacteristic(device).
+      then(characteristic => startNotifications(characteristic)).
+      catch(error => log(error));
 }
 
 // Кэш объекта характеристики
 let characteristicCache = null;
-let charArray = null;
-// let ioCharacteristicCache = null;
-let serviceInstance = null;
-
-function getPrimaryService(device) {
-  return serviceInstance
-    ? Promise.resolve(serviceInstance)
-    : device.gatt.connect()
-      .then(server => {
-        log('GATT server connected, getting service...');
-        serviceInstance = server ;
-        return server.getPrimaryService(0xAA80);
-      });
-}
-
-function readCharacteristic(device, param) {
-  return getPrimaryService(device)
-    .then(service => {
-      return service.getCharacteristic(param);
-    });
-}
-
-function showValues(device) {
-  var chars = [0xAA81, 0xAA82, 0xAA83];
-  if (!charArray) {
-    for (var i in chars) {
-      readCharacteristic(device, chars[i])
-        .then(characteristic => {
-          if (!charArray) {
-            charArray = {};
-          }
-          var uuid = characteristic.uuid;
-          Promise.resolve(characteristic.readValue())
-            .then(value => {
-              var _val;
-              switch(uuid) {
-                case '0000aa81-0000-1000-8000-00805f9b34fb':
-                  _val = value.getUint32(0);
-                  break;
-                case '0000aa82-0000-1000-8000-00805f9b34fb':
-                  _val = value.getInt16(0);
-                  $('#input').attr('data-uuid', uuid);
-                  $('#input').val(_val);
-                  break;
-                case '0000aa83-0000-1000-8000-00805f9b34fb':
-                  _val = value.getUint8(0);
-                  $('#startBtn')
-                    .attr('data-uuid', uuid)
-                    .attr('data-value', 3);
-                  $('#stopBtn')
-                    .attr('data-uuid', uuid)
-                    .attr('data-value', 5);
-                  break;
-              }
-              charArray[uuid] = {
-                characteristic: characteristic,
-                value: _val
-              };
-              log(uuid + ': ' + _val);
-            });
-        });
-    }
-  }
-}
 
 // Подключение к определенному устройству, получение сервиса и характеристики
 function connectDeviceAndCacheCharacteristic(device) {
@@ -181,80 +108,69 @@ function connectDeviceAndCacheCharacteristic(device) {
 
   log('Connecting to GATT server...');
 
-  // return device.gatt.connect()
-  //   .then(server => {
-  //     log('GATT server connected, getting service...');
-  //     serviceInstance = server ;
-  //     return server.getPrimaryService(0xAA80);
-  //   })
-  return getPrimaryService(device)
-    .then(service => {
-      log('Service found, getting characteristic...');
+  return device.gatt.connect().
+      then(server => {
+        log('GATT server connected, getting service...');
 
-      return service.getCharacteristic(0xAA81);
-    })
-    .then(characteristic => {
-      log('Characteristic found');
-      characteristicCache = characteristic;
+        return server.getPrimaryService(0xAA80);
+      }).
+      then(service => {
+        log('Service found, getting characteristic...');
 
-      return characteristicCache;
-    });
-// 	   .then(_ => {
-//         return serviceInstance.getPrimaryService(0xAA64);
-// 		log('getting service...');
-// 		then(newService => {
-// 			log('Service found, getting characteristic...');
-// 			return newService.getCharacteristic(0xAA65);
-// 		})
-// 		.then(newCharacteristic => {
-// 			log('Characteristic found');
-// 			ioCharacteristicCache = newCharacteristic;
-// //			return ioCharacteristicCache;
-// 		})
-//       });
+        return service.getCharacteristic(0xAA81);
+      }).
+      then(characteristic => {
+        log('Characteristic found');
+        characteristicCache = characteristic;
+
+        return characteristicCache;
+      });
 }
 
 // Включение получения уведомлений об изменении характеристики
 function startNotifications(characteristic) {
   log('Starting notifications...');
 
-  return characteristic.startNotifications()
-    .then(() => {
-      log('Notifications started');
+  return characteristic.startNotifications().
+      then(() => {
+        log('Notifications started');
 
-      // Добавленная строка
-      characteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);		
-    });
+        // Добавленная строка
+        characteristic.addEventListener('characteristicvaluechanged',
+            handleCharacteristicValueChanged);		
+      });
 }
 
 // Вывод в терминал
 function log(data, type = '') {
-  terminalContainer.insertAdjacentHTML('beforeend', '<div' + (type ? ' class="' + type + '"' : '') + '>' + data + '</div>');
+  terminalContainer.insertAdjacentHTML('beforeend',
+      '<div' + (type ? ' class="' + type + '"' : '') + '>' + data + '</div>');
 }
 
 // Отключиться от подключенного устройства
 function disconnect() {
   if (deviceCache) {
     log('Disconnecting from "' + deviceCache.name + '" bluetooth device...');
-    deviceCache.removeEventListener('gattserverdisconnected', handleDisconnection);
+    deviceCache.removeEventListener('gattserverdisconnected',
+        handleDisconnection);
 
     if (deviceCache.gatt.connected) {
       deviceCache.gatt.disconnect();
       log('"' + deviceCache.name + '" bluetooth device disconnected');
     }
     else {
-      log('"' + deviceCache.name + '" bluetooth device is already disconnected');
+      log('"' + deviceCache.name +
+          '" bluetooth device is already disconnected');
     }
   }
 
   // Добавленное условие
   if (characteristicCache) {
-    characteristicCache.removeEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
+    characteristicCache.removeEventListener('characteristicvaluechanged',
+        handleCharacteristicValueChanged);
     characteristicCache = null;
   }
-
-  charArray = null;
-  serviceInstance = null;
+  
   deviceCache = null;
 }
 
@@ -263,32 +179,15 @@ function handleCharacteristicValueChanged(event) {
   log(event.target.value.getUint32(0)/100, 'in'); // (0, littleEndian)
 }
 
-function int16ToInt8Array(value) {
-  // we want to represent the input as a 8-bytes array
-  var byteArray = [(value >> 8) & 0xFF, value & 0xFF];
-
-  return new Int8Array(byteArray);
-};
-
 // Отправить данные подключенному устройству
-function send() {
-  var uuid = $('#input').attr('data-uuid');
-  var value = $('#input').val();
-  var characteristic = charArray[uuid].characteristic;
-  var converted = int16ToInt8Array(value);
-  characteristic.writeValue(converted);
-  log('Set ' + uuid + ' value ' + value);
-}
-/*
 function send(data) {
   data = String(data);
 
   if (!data || !characteristicCache) {
-	  log('err');
+	log('err');
     return;
   }
 
   writeToCharacteristic(characteristicCache, data);
   log(data, 'out');
 }
-*/
